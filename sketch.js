963adc0026aa9b0674b03594a048b85f167aab4d
Millaray Dinamarca C. <millaray.dinamarca@ug.uchile.cl>
2025-09-01 15:35:03 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,25 +1,24 @@
-  var valor = document.getElementById("pecinco").offsetWidth;
+var valor = document.getElementById("pecinco").offsetWidth;
 
             window.addEventListener("resize", function () {
                 valor = document.getElementById("pecinco").offsetWidth;
             });
 
-            function setup() {
-                createCanvas(valor, valor, WEBGL).parent("#pecinco");
-                select("#nombre").html("Millaray Dinamarca");
-                select("#aprendizaje").html("a programar cosas bonitas");
-            }
-            function draw() {
-                background(237,93,143);
-                orbitControl();
-                pointLight (255,255,255, valor/3, valor/5, 50);
-                specularMaterial(255);
-                shininess(50);
-                metalness(1);
-                fill(70,120,255);
-                torus(valor/4, valor/4);
-            }
-
-            function windowResized() {
-                resizeCanvas(valor, valor);
-            }
+function setup() {
+        createCanvas(valor, valor, WEBGL).parent("#pecinco");
+        select("#nombre").html("Millaray Dinamarca");
+        select("#aprendizaje").html("a programar cosas bonitas");
+}
+function draw() {
+        background(237,93,143);
+        orbitControl();
+        pointLight (255,255,255, valor/3, valor/5, 50);
+        specularMaterial(255);
+        shininess(50);
+        metalness(1);
+        fill(70,120,255);
+        torus(valor/4, valor/4);
+}
+function windowResized() {
+        resizeCanvas(valor, valor);
+}
